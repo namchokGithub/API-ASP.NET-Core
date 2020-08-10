44efe98623f3bb7ebd554c7b14b6341fb49ce4a3
Namchok <pech4751@gmail.com>
2020-08-10 22:51:37 +0700

    add new file

---
+++ API-ASP.NET-Core/Data/IEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API_ASP.NET_Core.Data
+{
+    public class IEntity
+    {
+    }
+}
