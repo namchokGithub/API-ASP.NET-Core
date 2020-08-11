379e5d508a22b9278e09d748d41dddfd2caaf703
namchokGithub <pech4751@gmail.com>
2020-08-11 15:19:10 +0700

    update

---
+++ RepositoryPattern/Data/MyDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace RepositoryPattern.Models
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext (DbContextOptions<MyDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<RepositoryPattern.Models.Movie> Movie { get; set; }
+    }
+}
